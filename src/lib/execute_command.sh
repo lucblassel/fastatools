92de348d4e963382086834ef19d9c5ae80f5df3e
Luc Blassel <luc.blassel@gmail.com>
2021-12-17 13:07:22 +0100

    better var names

--- src/lib/execute_command.sh
+++ src/lib/execute_command.sh
@@ -3,43 +3,49 @@
 execute_command() {
     # Takes care of IO
     # $1 is the command (required)
+    cmd="$1"
     # $2 is the input file (optional)
+    input="$2"
     # $3 is the output file (optional)
+    output="$3"
     # $4 is a piped command (optional)
+    pipe="$4"
     
-    if [[ -z $4 ]]
+    
+    
+    if [[ -z $pipe ]]
     then
-        if [[ -z $2 ]]
+        if [[ -z $input ]]
         then
-            if [[ -z $3 ]]
+            if [[ -z $output ]]
             then
-                eval "$1"
+                eval "$cmd"
             else
-                eval "$1" > "$3"
+                eval "$cmd" > "$output"
             fi
         else
-            if [[ -z $3 ]]
+            if [[ -z $output ]]
             then
-                eval "$1" "$2"
+                eval "$cmd" "$input"
             else
-                eval "$1" "$2" > "$3"
+                eval "$cmd" "$input" > "$output"
             fi
         fi
     else
-        if [[ -z $2 ]]
+        if [[ -z $input ]]
         then
-            if [[ -z $3 ]]
+            if [[ -z $output ]]
             then
-                eval "$1" "|" "$4"
+                eval "$cmd" "|" "$pipe"
             else
-                eval "$1" "|" "$4" > "$3"
+                eval "$cmd" "|" "$pipe" > "$output"
             fi
         else
-            if [[ -z $3 ]]
+            if [[ -z $output ]]
             then
-                eval "$1" "$2" "|" "$4"
+                eval "$cmd" "$input" "|" "$pipe"
             else
-                eval "$1" "$2" "|" "$4" > "$3"
+                eval "$cmd" "$input" "|" "$pipe" > "$output"
             fi
         fi
     fi
