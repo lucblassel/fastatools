61136eaeadd450f189dc066677027fe40d80ee7b
Luc Blassel <luc.blassel@gmail.com>
2021-12-17 12:15:34 +0100

    adding sum mode to length command

--- src/length_command.sh
+++ src/length_command.sh
@@ -7,10 +7,14 @@ SCRIPTMIN='BEGIN { RS=">"; FS="\n"; OFS="\t"; min=-1; } NR>1{ linelen=0; for (i=
 
 SCRIPTMEAN='BEGIN { RS=">"; FS="\n"; OFS="\t"; total=0; count=0; } NR>1{ linelen=0; for (i=2; i<NF; i++) { total=total+length($i) } count++ } END{ print total/count }'
 
+SCRIPTSUM='BEGIN { RS=">"; FS="\n"; OFS="\t"; } NR>1{ linelen=0; for (i=2; i<NF; i++) { linelen=linelen+length($i) } total += linelen } END {print total}'
+
+FIELD=1
 case ${args[--mode]} in
     
     each)
         SCRIPT=$SCRIPTEACH
+        FIELD=2
     ;;
     min)
         SCRIPT=$SCRIPTMIN
@@ -21,6 +25,14 @@ case ${args[--mode]} in
     mean)
         SCRIPT=$SCRIPTMEAN
     ;;
+    sum)
+        SCRIPT=$SCRIPTSUM
+    ;;
 esac
 
-execute_command "awk '$SCRIPT'" "${args[--input]}" "${args[--output]}"
+if [[ -n ${args[--readable]} ]]
+then
+    execute_command "awk '$SCRIPT'" "${args[--input]}" "${args[--output]}" "numfmt --to=si --field=$FIELD --format=\"%.2f\""
+else
+    execute_command "awk '$SCRIPT'" "${args[--input]}" "${args[--output]}"
+fi
