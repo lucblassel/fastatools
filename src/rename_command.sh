852c05c9deebc7d29bf1bfa824aa6b779f386c22
Luc Blassel <luc.blassel@gmail.com>
2022-01-14 17:10:05 +0100

    handle spaces in names for rename command

--- src/rename_command.sh
+++ src/rename_command.sh
@@ -1,15 +1,16 @@
 
 if [[ -n "${args[--file]}" ]]
 then
-    SCRIPT="BEGIN {while (getline < \"'\"${args[--file]}\"'\") { split(\$0, fields); renamer[fields[1]] = fields[2]; } close(\"'\"${args[--file]}\"'\"); RS=\">\"; FS=\"\n\"; } NR>1 { print \">\" renamer[\$1]; for (i=2; i<NF; i++) { print \$i } }"
+    SCRIPT="BEGIN {while (getline < \"'\"${args[--file]}\"'\") { split(\$0, fields, \">\"); renamer[fields[1]] = fields[2]; } close(\"'\"${args[--file]}\"'\"); RS=\">\"; FS=\"\n\"; } NR>1 { print \">\" renamer[\$1]; for (i=2; i<NF; i++) { print \$i } }"
 elif [[ -n "${args[--regex]}" ]]
 then
     if [[ -z "${args[--substitute]}" ]]
     then
-        echo "You must specify a replace group with --substitute when using regex"
-        exit 1;
+        S=""
+    else
+        S="${args[--substitute]}"
     fi
-    SCRIPT="BEGIN { RS=\">\"; FS=\"\n\"; } NR>1 { gsub(/${args[--regex]}/, \"${args[--substitute]}\", \$1); print \">\" \$1; for (i=2; i<NF; i++) { print \$i } }"
+    SCRIPT="BEGIN { RS=\">\"; FS=\"\n\"; } NR>1 { gsub(/${args[--regex]}/, \"$S\", \$1); print \">\" \$1; for (i=2; i<NF; i++) { print \$i } }"
 else
     echo "You must specify file or regex to rename sequences"
     exit 1;
