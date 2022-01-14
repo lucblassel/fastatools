fe1e5b2cca52e2276c8e8a684538fc4d1412eab5
Luc Blassel <luc.blassel@gmail.com>
2022-01-14 17:09:40 +0100

    more efficient names command

--- src/names_command.sh
+++ src/names_command.sh
@@ -1,4 +1,4 @@
 
 
 SCRIPT='BEGIN { RS=">"; FS="\n" } NR>1{ print $1 }'
-execute_command "awk '$SCRIPT'" "${args[--input]}" "${args[--output]}"
+execute_command "grep '>'" "${args[--input]}" "${args[--output]}" "cut -c 2-"
