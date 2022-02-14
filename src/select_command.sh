894d2fe219dc67886eba7056c9505b08d89cc4ea
Luc Blassel <luc.blassel@gmail.com>
2022-02-14 12:41:05 +0100

    Changing select command to awk array for names in file

--- src/select_command.sh
+++ src/select_command.sh
@@ -6,23 +6,63 @@ function join_by {
     echo "$*";
 }
 
-if [[ -n ${args[--file]} ]]
+if [[ -n "${args[--file]}" ]]
 then
-    joined="($(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/|/g;s/|$//' "${args[--file]}" ))"
-elif [[ -n ${args[--regex]} ]]
-then
-    joined="${args[--regex]}"
+    if [[ ${args[--exclude]} ]]
+    then
+        operator="!="
+    else
+        operator="=="
+    fi
+    SCRIPT="
+    BEGIN {
+        while ( getline < \"${args[--file]}\" ) {
+            names[\$0] = 1;
+        }
+        RS=\">\"; 
+        FS=\"\n\";
+    } NR>1 {
+        if (names[\$1] $operator 1) {
+            print \">\" \$1; 
+            for (i=2; i<NF; i++) { 
+                print \$i ;
+            } 
+        }
+    }
+    "
 else
-    joined="($(join_by "|" ${other_args[*]} ))"
+    if [[ -n "${args[--regex]}" ]]
+    then
+        joined="${args[--regex]}"
+    else
+        joined="($(join_by "|" ${other_args[*]} ))"
+    fi
+    if [[ ${args[--exclude]} ]]
+    then
+        operator="!~"
+    else
+        operator="~"
+    fi
+    SCRIPT="BEGIN { RS=\">\"; FS=\"\n\" } NR>1 { if (\$1 $operator /^$joined\$/) { print \">\"\$1; for (i=2; i<NF; i++) { print \$i } } }"
 fi
 
-if [[ ${args[--exclude]} ]]
-then
-    operator="!~"
-else
-    operator="~"
-fi
+# if [[ -n ${args[--file]} ]]
+# then
+#     joined="($(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/|/g;s/|$//' "${args[--file]}" ))"
+# elif [[ -n ${args[--regex]} ]]
+# then
+#     joined="${args[--regex]}"
+# else
+#     joined="($(join_by "|" ${other_args[*]} ))"
+# fi
 
-SCRIPT="BEGIN { RS=\">\"; FS=\"\n\" } NR>1 { if (\$1 $operator /^$joined\$/) { print \">\"\$1; for (i=2; i<NF; i++) { print \$i } } }"
+# if [[ ${args[--exclude]} ]]
+# then
+#     operator="!~"
+# else
+#     operator="~"
+# fi
+
+# SCRIPT="BEGIN { RS=\">\"; FS=\"\n\" } NR>1 { if (\$1 $operator /^$joined\$/) { print \">\"\$1; for (i=2; i<NF; i++) { print \$i } } }"
 
 execute_command "awk '$SCRIPT'" "${args[--input]}" "${args[--output]}"
